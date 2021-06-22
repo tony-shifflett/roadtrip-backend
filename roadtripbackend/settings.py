71e01557689332a83db3430f10573c25b8011ca0
Tony Shifflett <charles.l.shifflett@gmail.com>
2021-06-22 08:34:25 -0500

    updated ALLOWED_HOSTS

--- roadtripbackend/settings.py
+++ roadtripbackend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-90b)jh7$!1iiu%xb7-or8suc9z_%1%egnttq-6bitts6qu-jdy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
